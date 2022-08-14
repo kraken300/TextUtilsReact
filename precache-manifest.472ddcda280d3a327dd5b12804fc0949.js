99e11e535af7c9b605984913d58df32835227671
kraken300 <shivamsingh113355@gmail.com>
2022-08-14 18:07:21 +0530

    Updates

---
+++ precache-manifest.472ddcda280d3a327dd5b12804fc0949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d73d7806be75b9ad36cb",
+    "url": "/TextUtilsReact/static/css/main.9fd5bc57.chunk.css"
+  },
+  {
+    "revision": "d73d7806be75b9ad36cb",
+    "url": "/TextUtilsReact/static/js/main.d73d7806.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtilsReact/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "47a61aeb0ce2eb63fb5a",
+    "url": "/TextUtilsReact/static/js/2.47a61aeb.chunk.js"
+  },
+  {
+    "revision": "03c19bc6ae83b0ea9d66",
+    "url": "/TextUtilsReact/static/js/runtime~main.03c19bc6.js"
+  },
+  {
+    "revision": "23a90ab4a6256003a9dd93af5dd8b3a5",
+    "url": "/TextUtilsReact/index.html"
+  }
+];
